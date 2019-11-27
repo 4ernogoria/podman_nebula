738969ba6e6ad5e221c244fe444dc3f1d04f4b84
frolovav <chehome@mail.ru>
2019-11-27 16:18:53 +0300

    commented redundant

--- nebula_cont/stnebula.sh
+++ nebula_cont/stnebula.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #sudo chown -R root:9869 /etc/one
-sudo chown -R 9869:9869 /var/lib/one
+#sudo chown -R 9869:9869 /var/lib/one
 ls -la /etc/one/
 ls -la /var/lib/one
 /usr/bin/oned 
@@ -8,4 +8,4 @@ echo "oned service is started"
 ping localhost -c 3 -i 5 2>&1 >/dev/null
 echo "mm_sched is started, enjoy"
 /usr/bin/mm_sched
-/bin/bash
+#/bin/bash
