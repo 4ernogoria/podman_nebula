98ed4bcd31329496bc2ee74f1e7c8a8d138d67be
4ernogoria <chehome@mail.ru>
2019-12-01 20:40:54 +0300

    added a commented line, for preserving only necessary files in /var/lib/one/.one

--- nebula_cont/stnebula.sh
+++ nebula_cont/stnebula.sh
@@ -3,9 +3,11 @@
 #sudo chown -R 9869:9869 /var/lib/one
 ls -la /etc/one/
 ls -la /var/lib/one
+#if mounted folders contain the configs from previous installations uncomment next lines, or nebula's gonna complain about the existing one_auth 
+#cd /var/lib/one/.one && ls | grep -v one_auth | xargs rm -rfv
 /usr/bin/oned 
 echo "oned service is started"
 ping localhost -c 3 -i 5 2>&1 >/dev/null
 echo "mm_sched is started, enjoy"
 /usr/bin/mm_sched
-#/bin/bash
+tail -f /var/log/one/one*
