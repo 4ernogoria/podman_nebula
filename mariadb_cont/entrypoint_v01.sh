96638aef74f64ccc36c2dfff64f170dc2af375ee
4ernogoria <chehome@mail.ru>
2019-12-01 19:46:48 +0300

    seems working

--- mariadb_cont/entrypoint_v01.sh
+++ mariadb_cont/entrypoint_v01.sh
@@ -46,8 +46,7 @@ if [ "$1" = 'mysqld_safe' ]; then
 			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$tempSqlFile"
 			
 			if [ "$MYSQL_DATABASE" ]; then
-				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> "$tempSqlFile" && /
-                                echo "SET GLOBAL TRANSACTION ISOLATION LEVEL READ COMMITTED;" >> "$tempSqlFile"
+				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';" >> "$tempSqlFile" && echo "SET GLOBAL TRANSACTION ISOLATION LEVEL READ COMMITTED;" >> "$tempSqlFile"
 			fi
 		fi
 		
@@ -59,3 +58,4 @@ if [ "$1" = 'mysqld_safe' ]; then
 fi
 
 exec "$@"
+tail -f /var/log/mariadb/mariadb.log
